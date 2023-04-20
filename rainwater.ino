7635f2f30d7db08b21f5ce6eb141c719d52b357b
vedantkadam <83024561+vedantkadam@users.noreply.github.com>
2023-04-20 19:28:11 +0530

    Update rainwater.ino

--- rainwater.ino
+++ rainwater.ino
@@ -14,10 +14,11 @@ char pass[] = "";//Enter your WIFI password
 BlynkTimer timer;
 
 #define RAIN_SENSOR_PIN A0
-#define RAIN_SENSOR_THRESHOLD 800 // adjust this value based on your sensor
+#define RAIN_SENSOR_THRESHOLD 850 // adjust this value based on your sensor
 const int redLED = D3;
 const int buzpin = D5; // change this to a different pin if necessary
 int flag = 0;
+#define VPIN_BUTTON_2 V1
 
 #define ON HIGH
 #define OFF LOW
@@ -37,6 +38,7 @@ void loop()
 
   if (sensorValue < RAIN_SENSOR_THRESHOLD) {
     Blynk.logEvent("test_rain");
+    Blynk.virtualWrite(VPIN_BUTTON_2, "Water Level is Above Average !!");
     digitalWrite(redLED, HIGH);
     digitalWrite(D3, HIGH);
     tone(buzpin, 50);
@@ -46,7 +48,8 @@ void loop()
     flag = 0;
     noTone(buzpin); // make sure this function is called
     digitalWrite(redLED, LOW);
-     digitalWrite(D3, LOW);
+    digitalWrite(D3, LOW);
+    Blynk.virtualWrite(VPIN_BUTTON_2, "Water Level is Normal");
   }
 
   Blynk.virtualWrite(V0, sensorValue);
